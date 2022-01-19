614079f639726f7eb23360bbce4346bdd823750c
KangChou <kangchou666@gmail.com>
2022-01-20 00:38:51 +0800

    Update run_push.sh

--- run_push.sh
+++ run_push.sh
@@ -3,6 +3,6 @@ git init
 git add ./
 git commit -m "update"
 git branch -M main
-git remote set-url origin https://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@github.com/KangChou/Cver4s.git
+git remote set-url origin https://github.com/KangChou/Cver4s.git
 git push -u origin main
 
